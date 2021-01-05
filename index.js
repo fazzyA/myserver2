dc7e8a8860e94666bf74cdbf3d79065dd190e819
ddd <ddd>
2021-01-05 09:48:49 +0500

    signup in express

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const bodyParser = require('body-parser');
+const { resolveSoa } = require('dns');
 const app = express();
 const PORT = 4000
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, (req,res)=>{
 console.log('server is running at', PORT)
 })
-// app.get('/',(req,res)=>{
-// res.send('<h1>My first App</h1>')
-// })
-// app.get('/',(req,res)=>{
-//     res.sendFile(path.join(__dirname,'public','index.html'))
-// })
+let users = [
+    {name:'Faiza',id:1,email:'faz.pak@gmail.com',password:'1111'},
+    {name:'Abeer',id:1,email:'abeer@gmail.com',password:'2222'}    
+]
+//// routes
+app.get('/signup',(req,res)=>{
+res.sendFile(path.join(__dirname,'registration','signup.html'))
+})
+app.post('/signup',(req,res)=>{
+   // res.send(req.body)
+  // console.log(req.body)
+  let {email, name, password} = req.body
+  let found = users.some((item)=>item.email == email)
+  if(found){
+      res.send('<h1>User already existed</h1>')
+  } else{
+      users.push({name,email,password,id:users.length+1})
+     // res.send('user added')
+     res.sendFile(path.join(__dirname, 'registration','login.html'))
+  }
 
-// app.get("/aboutus", (req,res)=>{
-//     //res.send('<h1>Aboutus</h1>')
-//     res.sendFile(path.join(__dirname, 'public','aboutus.html'))
-// })
-// app.get("/contact", (req,res)=>{
-//     //res.send('<h1>Aboutus</h1>')
-//     res.sendFile(path.join(__dirname, 'public','contact.html'))
-// })
-
-app.use(express.static(path.join(__dirname,'public')));
-app.use(express.static(path.join(__dirname,'registration')))
+})
