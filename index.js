6e2a8ad4693903a9814a1e619947f32a9cce1338
fff <fff>
2021-01-06 11:15:30 +0500

    api with router

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const users = require('./Users')
 const app = express();
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = 4000
-app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, (req,res)=>{
 console.log('server is running at', PORT)
 })
-console.log(users)
-app.get('/api/users', (req,res)=>{
-    res.json(users)
-})
-app.get('/api/users/:id', (req,res)=>{
-    let id = req.params.id
-    console.log(id)
-    let result = users.filter((item)=>item.id == id)
-    res.json(result[0])
-})
+app.use('/api/users', require('./routes/api/users'))
+
