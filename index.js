bc56ba2f30e6058ced2282a8f70c3df41a4c3db4
fff <fff>
2021-01-06 10:02:39 +0500

    api

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const { resolveSoa } = require('dns');
+const users = require('./Users')
 const app = express();
+
 const PORT = 4000
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, (req,res)=>{
 console.log('server is running at', PORT)
 })
-let users = [
-    {name:'Faiza',id:1,email:'faz.pak@gmail.com',password:'1111'},
-    {name:'Abeer',id:1,email:'abeer@gmail.com',password:'2222'}    
-]
-//// routes
-app.get('/signup',(req,res)=>{
-res.sendFile(path.join(__dirname,'registration','signup.html'))
+console.log(users)
+app.get('/api/users', (req,res)=>{
+    res.json(users)
 })
-app.get('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname,'registration','login.html'))
-    })
-    
-app.post('/signup',(req,res)=>{
-   // res.send(req.body)
-  // console.log(req.body)
-  let {email, name, password} = req.body
-  let found = users.some((item)=>item.email == email)
-  if(found){
-      res.send('<h1>User already existed</h1>')
-  } else{
-      users.push({name,email,password,id:users.length+1})
-     res.redirect('/login')
-  }
-
+app.get('/api/users/:id', (req,res)=>{
+    let id = req.params.id
+    console.log(id)
+    let result = users.filter((item)=>item.id == id)
+    res.json(result[0])
 })
